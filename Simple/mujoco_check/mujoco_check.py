62e4f3d796c4ae55a7935db7424f440966c45b1d
Joker <joker012280@gmail.com>
2022-07-18 17:15:56 +0900

    Update

---
+++ Simple/mujoco_check/mujoco_check.py
@@ -0,0 +1,29 @@
+# import random
+# from gym.envs.mujoco.half_cheetah_v4 import HalfCheetahEnv
+# env = HalfCheetahEnv()
+#
+# for _ in range(100):
+# 	env.render()
+# 	action = [random.randint(0,1) for _ in range(env.action_space.shape[0])]
+# 	_, _, _, _ = env.step(action)
+#
+# env.close()
+# env.close()
+import mujoco_py
+import os
+mj_path = mujoco_py.utils.discover_mujoco()
+xml_path = os.path.join(mj_path, 'model', 'humanoid.xml')
+model = mujoco_py.load_model_from_path(xml_path)
+sim = mujoco_py.MjSim(model)
+
+print(sim.data.qpos)
+# [0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]
+
+sim.step()
+print(sim.data.qpos)
+# [-2.09531783e-19  2.72130735e-05  6.14480786e-22 -3.45474715e-06
+#   7.42993721e-06 -1.40711141e-04 -3.04253586e-04 -2.07559344e-04
+#   8.50646247e-05 -3.45474715e-06  7.42993721e-06 -1.40711141e-04
+#  -3.04253586e-04 -2.07559344e-04 -8.50646247e-05  1.11317030e-04
+#  -7.03465386e-05 -2.22862221e-05 -1.11317030e-04  7.03465386e-05
+#  -2.22862221e-05]
